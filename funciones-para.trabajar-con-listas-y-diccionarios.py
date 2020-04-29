59d25ac5809cbc4be5038e8a3c35a152b794f058
linux <linux@linux-G41T-M>
2020-04-29 19:59:44 -0400

    funciones para trabajar con listas y diccionarios

---
+++ funciones-para.trabajar-con-listas-y-diccionarios.py
@@ -0,0 +1,35 @@
+#Unir dos listas en una sola
+lista = ['pera','manzana','mango']
+lista2 = ['papaya', 'melon', 'sandia']
+
+lista3 = lista + lista2
+
+print(lista3)
+
+#Cambiar el valor de un item en una lista:
+
+lista = ['pera', 'manzana', 'fresa']
+lista[1] = 'perro'
+print(lista) #['pera', 'perro', 'fresa']
+
+#Eliminar Elemento de una lista:
+
+del lista[0]
+print(lista) #['perro', 'fresa']
+print(len(lista)) #2
+
+#programa que agrega elementos a una lista
+
+#reiniciamos la variable lista la ponemos igual a una lista vacia
+lista = []
+datos = ''
+
+while True:
+    datos = input('Desea agregar mas datos a la lista? deje en blanco para detener: ')
+    strdatos = str(datos)
+    if strdatos == '':
+        break
+    lista.append(strdatos)
+
+    for i in lista:
+        print(i)
