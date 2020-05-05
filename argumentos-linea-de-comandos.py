31f9b5bcaed6af375d024d6f938a46e41dd7700f
kenderperez <kenderperez1995@gmail.com>
2020-05-05 13:10:18 -0400

    para trabajar con los argumentos pasados en la linea de comandos al ejecutar el scrip

---
+++ argumentos-linea-de-comandos.py
@@ -0,0 +1,14 @@
+import sys
+
+argumentos = sys.argv[1:]
+argumento1 = str(argumentos[0])
+argumento2 = str(argumentos[1])
+print(argumentos)
+print(argumento1, argumento2)
+
+#ejecutar  con: python3 argumentos-linea-de-comandos.py hola mundo
+#salida: ['hola', 'mundo']
+#salida2: hola mundo
+
+
+
