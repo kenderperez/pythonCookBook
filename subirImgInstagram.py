cac0f9706e153bd03da415e64f320c4f61181208
kenderperez <kenderperez1995@gmail.com>
2020-05-24 16:27:20 -0400

    Update subirImgInstagram.py

--- subirImgInstagram.py
+++ subirImgInstagram.py
@@ -2,15 +2,15 @@ from instapy_cli import client
 user = 'usuario de instagram'
 passw = 'clave de instagram'
 text = 'texto del post'
-img = 'ruta de la imagen'
+img = 'imagen.png'
 
 
-def subirImagen(usuario, password, texto, imagen):
+def subirImagen(texto, imagen, usuario=user, password=passw):
     
     with client(usuario, password) as cli:
         cli.upload(imagen, texto)
         
         
-#para subir una imagen a instagram vasta con poner
+#para subir una imagen a instagram vasta con ejecutar la funcion subirImagen y espesificar el texto del post y el nombre de la imagen ej: imagen.png
 
-subirImagen(user, passw, text, img)
+subirImagen(text, img)
